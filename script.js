7991aef556fbc2c3d0d7fccb9ede2a992dc88a9d
Niles <nileschampeaux@gmail.com>
2022-04-21 17:24:37 -0700

    Create button to clear grid

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const container = document.querySelector("#container");
 
-container.classList.add("flex-container");
+// clear grid button
+const button = document.createElement("button");
+document.body.insertBefore(button, document.body.firstChild);
+button.innerText = "Clear Grid";
+button.classList.add("clear-button");
 
+button.addEventListener("click", function () {
+  container.remove("div");
+  //   let gridSize = parseInt(
+  //     prompt("How many squares per side in the new grid?", "")
+  //   );
+  //   while (gridSize > 100) {
+  //     gridSize = parseInt(prompt("Set a value of 100 or less"));
+  //   }
+
+  //   // set functionality creating new grid
+  //   for (let i = 0; i < gridSize * gridSize; i++) {
+  //     const div = document.createElement("div");
+  //     div.classList.add("square");
+  //     container.append(div);
+  //     div.addEventListener("mouseover", function () {
+  //       div.classList.add("hover");
+  //     });
+  //   }
+});
+
+// container
+container.classList.add("grid-container");
+
+// divs
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.classList.add("square");
@@ -13,7 +41,8 @@ for (let i = 0; i < 256; i++) {
 
 // add an event listener for each div that adds the hover attribute
 
-// what should happen:
-// -mouse hovers over div and the div changes color (and keeps that color, unless hovered over again(?))
-// -MDN: "movement in and out of an element"
-// -mouseover (bubbling?), mouseenter, mouseleave
+// ISSUE with .grid-container and possibly .square in style.css: need to adjust grid column values every prompt input
+
+// when prompting user, take input and use that number of divs as i in th for loop?
+// "you should be able to enter 64 and have a brand new 64x64 grid pop up...
+// without changing the totla amount of pixels used"
