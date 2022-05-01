19d0d09623da92b489869adba0090e8aa4dba8cd
Niles <nileschampeaux@gmail.com>
2022-05-01 12:30:47 -0700

    Add black and rainbow button structure

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ button.innerText = "Clear Grid";
 button.classList.add("clear-button");
 button.addEventListener("click", newGrid);
 
+// color buttons container
+const colorBtnContainer = document.createElement("div");
+document.body.appendChild(colorBtnContainer);
+colorBtnContainer.classList.add("color-btn-container");
+
+// black button
+const blackBtn = document.createElement("button");
+blackBtn.innerText = "Black";
+colorBtnContainer.appendChild(blackBtn);
+blackBtn.addEventListener("click", function () {
+  console.log("BLACK CLICKED!");
+});
+
+// rainbow button
+const rainbowBtn = document.createElement("button");
+rainbowBtn.innerText = "Rainbow";
+colorBtnContainer.appendChild(rainbowBtn);
+rainbowBtn.addEventListener("click", function () {
+  console.log("RAINBOW CLICKED!");
+});
+
 // grid display
 const h2 = document.createElement("h2");
 document.body.insertBefore(h2, button);
@@ -23,13 +44,13 @@ function buildGrid(size) {
     const div = document.createElement("div");
     container.append(div);
     div.addEventListener("mouseover", function () {
-      div.classList.add("hover");
+      div.classList.add("hover-black");
     });
   }
   h2.innerText = `${size}x${size} `;
 }
 
-// set default size of 16x16
+// call to set default size of 16x16
 buildGrid(16);
 
 // create new grid function
